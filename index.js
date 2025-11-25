56d1a5ecf56f02f93044b86e66d5711999d63fb6
veveethan <veveethan.it22@bitsathy.ac.in>
2025-11-25 23:07:50 +0530

    added js file

---
+++ index.js
@@ -0,0 +1,16 @@
+// Simple message print
+console.log("Hello, this is my simple JavaScript code!");
+
+// Adding two numbers
+let a = 10;
+let b = 20;
+let sum = a + b;
+
+console.log("Sum is:", sum);
+
+// Simple function
+function greet(name) {
+    return "Hello " + name;
+}
+
+console.log(greet("Veveethan"));
